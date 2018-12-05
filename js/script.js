a800544f6463250b7e26b4bf398ab6fb45a01529
nextbendev <45582717+nextbendev@users.noreply.github.com>
2018-12-05 09:57:30 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+/*blog*/
+
+
+function rand(min, max) {
+  return Math.floor(Math.random() * max) + min;
+}
+
+document.querySelectorAll('.post:not(.featured)').forEach((post) => {
+  post.querySelector('.image').style.backgroundImage = `url("https://unsplash.it/300/300/?image=${rand(100, 1000)}")`;
+});
+
+
+/*end blog*/
+
