f8653d7b105b1aa2749ff6f5e076f6a359f55533
Ben Sommervlle <Reverie@Juice.local>
2019-05-22 19:51:10 -0400

    save ver

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get 'pages/contact'
   get "/pages/:page" => "pages#show"
 
-  root 'pages#home'
+  root "pages#home"
+
+  resources :pages
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
