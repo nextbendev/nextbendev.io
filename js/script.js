d4d1e5bb66302b317dedb1834eac09e13ef15530
nextbendev <gastronomicllc@gmail.com>
2019-06-27 16:34:52 -0400

    index

--- js/script.js
+++ js/script.js
@@ -1,15 +1,1 @@
 
-/*blog*/
-
-
-function rand(min, max) {
-  return Math.floor(Math.random() * max) + min;
-}
-
-document.querySelectorAll('.post:not(.featured)').forEach((post) => {
-  post.querySelector('.image').style.backgroundImage = `url("https://unsplash.it/300/300/?image=${rand(100, 1000)}")`;
-});
-
-
-/*end blog*/
-
