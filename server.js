3a31803439afbfe2f918738304dc92ec59924818
Ben Sommerville <bs@Bens-MacBook-Air.local>
2019-07-08 20:43:20 -0400

    game add

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
 const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs  = require('fs');
+
+const mimeTypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css",
+}
+http.createServer(function(req, res){
+  var utri = url.parse(req.url).pathname;
+  var fileName = path.join(process.cwd(), unescape(uri));
+  console.log('Loading'+ uri);
+  var status;
+
+})
 
 const hostname = '127.0.0.1';
 const port = 3000;
