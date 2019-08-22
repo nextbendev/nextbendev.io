7a3e31ffb91c79a7c98c0daf658d26168e062c68
Ben Sommerville <bs@Bens-MacBook-Air.local>
2019-08-22 15:34:57 -0400

    almost there

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
+var path = require('path');
 var express = require('express');
 var app = express();
-var path = require('path');
+var fs = require('fs');
 
-// viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+var dir = path.join(__dirname, 'public');
+
+var mime = {
+    html: 'text/html',
+    txt: 'text/plain',
+    css: 'text/css',
+    gif: 'image/gif',
+    jpg: 'image/jpeg',
+    png: 'image/png',
+    svg: 'image/svg+xml',
+    js: 'application/javascript'
+};
+
+app.get('*', function (req, res) {
+    var file = path.join(dir, req.path.replace(/\/$/, '/index.html'));
+    if (file.indexOf(dir + path.sep) !== 0) {
+        return res.status(403).end('Forbidden');
+    }
+    var type = mime[path.extname(file).slice(1)] || 'text/plain';
+    var s = fs.createReadStream(file);
+    s.on('open', function () {
+        res.set('Content-Type', type);
+        s.pipe(res);
+    });
+    s.on('error', function () {
+        res.set('Content-Type', 'text/plain');
+        res.status(404).end('Not found');
+    });
 });
 
-app.listen(3000);
+app.listen(3000, function () {
+    console.log('Listening on http://localhost:3000/');
+});
